d247c0210811a338ac848d93e08be58f4c0eb1aa
Nelson Georges <georson00@gmail.com>
2022-06-10 17:34:17 -0400

    Adding the 3 type of asteroids

--- Asteroid.py
+++ Asteroid.py
@@ -107,4 +107,90 @@ class Asteroid(FlyingObject):
             self.width = self.texture.width
             self.height = self.texture.height
             arcade.draw_texture_rectangle(self.center.x, self.center.y, self.width, self.height, self.texture, self.angle, 255)
-                 
+class SmallAsteroid(Asteroid):
+    def __init__(self):
+        super().__init__("images/meteorGrey_small1.png")
+        self.radius = SMALL_ROCK_RADIUS
+        self.spin = SMALL_ROCK_SPIN
+        self.speed = SMALL_ROCK_SPEED
+        self.center.x = random.randint(1, 50)
+        self.center.y = random.randint(1, 150)
+        self.direction = random.randint(1, 50)
+        self.velocity.dx = math.cos(math.radians(self.direction)) * self.speed
+        self.velocity.dy = math.cos(math.radians(self.direction)) * self.speed
+        
+    def break_apart(self, asteroids):
+        self.alive = False
+        
+class MediumAsteroid(Asteroid):
+    def __init__(self):
+        super().__init__("images/meteorGrey_med1.png")
+        self.radius = MEDIUM_ROCK_RADIUS
+        self.speed = MEDIUM_ROCK_SPEED
+        self.center.x = random.randint(1, 50)
+        self.center.y = random.randint(1, 150)
+        self.direction = random.randint(1, 50)
+        self.velocity.dx = math.cos(math.radians(self.direction)) * self.speed
+        self.velocity.dy = math.cos(math.radians(self.direction)) * self.speed
+        self.spin = MEDIUM_ROCK_SPIN
+    
+    def break_apart(self, asteroids):           
+         
+        # Create a small asteroid
+        sma_ast1 = SmallAsteroid()
+        sma_ast1.center.x = self.center.x
+        sma_ast1.center.y = self.center.y
+        sma_ast1.velocity.dy =  self.velocity.dy + 1.5
+        sma_ast1.velocity.dx =  self.velocity.dx + 1.5
+        
+        # Create a second small asteroid
+        sma_ast2 = SmallAsteroid()
+        sma_ast2.center.x = self.center.x
+        sma_ast2.center.y = self.center.y
+        sma_ast1.velocity.dy =  self.velocity.dy - 1.5
+        sma_ast1.velocity.dx =  self.velocity.dx - 1.5
+         
+        # Add the small asteroids to the ist of Asteroids
+        asteroids.append(sma_ast1)
+        asteroids.append(sma_ast2)
+        self.alive = False        
+        
+class LargeAsteroid(Asteroid):
+    def __init__(self):
+        super().__init__("images/meteorGrey_big1.png")
+        
+        self.radius = BIG_ROCK_RADIUS
+        self.center.x = random.randint(1, 50)
+        self.center.y = random.randint(1, 150)
+        self.direction = random.randint(1, 50)
+        self.speed = BIG_ROCK_SPEED
+        self.velocity.dx = math.cos(math.radians(self.direction)) * self.speed
+        self.velocity.dy = math.cos(math.radians(self.direction)) * self.speed
+        self.spin = BIG_ROCK_SPIN
+    
+        
+    def break_apart(self, asteroids):
+        
+        #create a medium asteroid
+        med_ast1 = MediumAsteroid()
+        med_ast1.center.x = self.center.x
+        med_ast1.center.y = self.center.y
+        med_ast1.velocity.dy = self.velocity.dy + 2
+         
+        # Create a second medium asteroid
+        med_ast2 = MediumAsteroid()
+        med_ast2.center.x = self.center.x
+        med_ast2.center.y = self.center.y
+        med_ast2.velocity.dy = self.velocity.dy - 2
+         
+        # Create a small asteroid
+        sma_ast = SmallAsteroid()
+        sma_ast.center.x = self.center.x
+        sma_ast.center.y = self.center.y
+        sma_ast.velocity.dx = self.velocity.dx + 5
+         
+        # Add the asteroids being created to the list of asteroids
+        asteroids.append(med_ast1)
+        asteroids.append(med_ast2)
+        asteroids.append(sma_ast)
+        self.alive = False                 
